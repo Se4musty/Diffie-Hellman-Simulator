b8f0bcf1d53ccd9ac8c8ffc8495547779f577346
Seamus Tynan <32597881+Sh4m00se3878@users.noreply.github.com>
2018-11-15 22:57:16 -0500

    First version, requires some testing.

--- dh_key_generator.py
+++ dh_key_generator.py
@@ -1,4 +1,18 @@
 from math import floor
+from temporary_client import Client
+
+
+def split_length(list_length):
+    return floor(list_length/2)
+
+
+def secret_comparison(clients):
+    prev = clients[0]
+    for client in clients:
+        if client.shared_secret != prev.shared_secret:
+            return False
+        prev = client
+    return True
 
 
 class KeyGen:
@@ -11,28 +25,32 @@ class KeyGen:
     def generate_keys(self, current_list, list_length, generator):
         if list_length == 1:
             # if 1 client left, send key and p once more then return
+            current_list[0].calculate_key(generator, self.p)
             return
         else:
             # Split the list into left and right (left is smaller due to floor).
             left_length = split_length(list_length)
             right_length = list_length - left_length
+
             left = current_list[:left_length]
             right = current_list[left_length:]
-            print(left)
-            print(right)
 
-            # for client in left
-                # key and p used here
+            # set resultant keys to begin as the generator.
+            left_key = generator
+            right_key = generator
 
-            # for client in right
-                # key and p used here
+            # Loop through both halves of the current list to create a shared key on each half.
+            for client in left:
+                # send key and p to client, and receive their calculated key.
+                left_key = client.calculate_key(left_key, self.p)
 
-            # generate_keys(left, left_length, right_key)
-            # generate_keys(right, right_length, left_keys)
+            for client in right:
+                # send key and p to client, and receive their calculated key.
+                right_key = client.calculate_key(right_key, self.p)
 
-
-def split_length(list_length):
-    return floor(list_length/2)
+            # Recurse through left and right halves until secret_key has been created.
+            self.generate_keys(left, left_length, right_key)
+            self.generate_keys(right, right_length, left_key)
 
 
 if __name__ == '__main__':
@@ -40,8 +58,16 @@ if __name__ == '__main__':
     For testing purposes
     """
     # Initialize clients to be put into connected list
-
-    connected = [1, 2]
-    gen = 3
+    connected = []
+    for i in range(2):
+        connected.append(Client())
+    print(connected[0].private_key)
+    # connected = [1, 2]
     k = KeyGen(71)
-    k.generate_keys(connected, len(connected), gen)
+    k.generate_keys(connected, len(connected), 3)
+
+    # Once secret keys are calculated, check if they are the same
+    if secret_comparison(connected):
+        print("Secret key {}, was correctly calculated for all clients!".format(connected[0].shared_secret))
+    else:
+        print("Secret key wasn't correctly calculated")
